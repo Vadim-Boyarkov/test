356fc20cde528a8aed881cfa48e0bdaad0d559f2
boyarkov_v <fluentzhiv@gmail.com>
2022-09-12 11:50:47 +0400

    готово

---
+++ chetnie_doN/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите любое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (number > 0)
+{
+while (i <= number)
+{
+int ost = i % 2;
+if (ost == 0 )
+{
+    Console.Write(i + " ");
+}
+i++;
+}
+}
+else
+{
+Console.WriteLine("Введено не верное значение");
+}
